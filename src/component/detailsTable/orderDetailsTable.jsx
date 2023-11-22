5e76acf1062a0f09fd43f108901296659026c737
123yael <119128695+123yael@users.noreply.github.com>
2023-11-22 11:56:48 +0200

    somthing

--- src/component/detailsTable/orderDetailsTable.jsx
+++ src/component/detailsTable/orderDetailsTable.jsx
@@ -1,5 +1,4 @@
 import * as React from 'react';
-import { PALLETE } from '../../config';
 import { useEffect } from 'react';
 import { useState } from 'react';
 import { GlobalTable } from '../globalTable';
